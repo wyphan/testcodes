9d9f10bc0c4da1392922e3651c9229dd1a441770
Wileam Phan <wphan@vols.utk.edu>
2021-05-17 17:09:18 -0400

    Add compiler-ifdefs code

---
+++ compiler-ifdefs/compiler_ifdefs.F90
@@ -0,0 +1,28 @@
+PROGRAM compiler_ifdefs
+  IMPLICIT NONE
+
+#ifdef __GFORTRAN__
+  CHARACTER(LEN=*), PARAMETER :: compiler = 'GCC GFortran'
+#endif /* __GFORTRAN__ */
+
+#ifdef __INTEL_COMPILER
+  CHARACTER(LEN=*), PARAMETER :: compiler = 'Intel Fortran'
+#endif /* __INTEL_COMPILER */
+
+#ifdef __PGI
+#ifdef __NVCOMPILER
+  ! Note: NVHPC also defines __PGI for backwards compatibility
+  CHARACTER(LEN=*), PARAMETER :: compiler = 'NVIDIA HPC Fortran'
+#else
+  CHARACTER(LEN=*), PARAMETER :: compiler = 'PGI Fortran'
+#endif /* __NVCOMPILER */
+#endif /* __PGI */
+
+#ifdef __FLANG
+  CHARACTER(LEN=*), PARAMETER :: compiler = 'LLVM Flang'
+#endif /* __FLANG */
+
+  PRINT *, 'Compiled with ', compiler
+
+  STOP
+END PROGRAM compiler_ifdefs
