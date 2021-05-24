09b39f579d9592add5993ae05f7c86d1e6bf9cb6
Wileam Yonatan Phan <wphan@vols.utk.edu>
2021-05-24 21:30:59 +0000

    Update dadd.hip.cpp

--- hip-dadd/dadd.hip.cpp
+++ hip-dadd/dadd.hip.cpp
@@ -13,7 +13,7 @@ using namespace std;
 }
 
 // Kernel to add two vectors
-__global__ void dadd( int N,
+__global__ void dadd( const int N,
                       const double* d_vecA,
                       const double* d_vecB,
                       double* d_vecC ) {
@@ -23,6 +23,8 @@ __global__ void dadd( int N,
 
   if ( tid < N )
     d_vecC[tid] = d_vecA[tid] + d_vecB[tid];
+
+  __syncthreads();
 
   return;
 
@@ -52,6 +54,9 @@ int main( int argc, char* argv[] ) {
   }
   double chk = 3.0;
 
+  cout << "h_A[" << N-1 << "] = " << h_A[N-1] << endl;
+  cout << "h_B[" << N-1 << "] = " << h_B[N-1] << endl;
+
   // Allocate vectors on device
   double* d_A = NULL;
   double* d_B = NULL;
@@ -70,8 +75,10 @@ int main( int argc, char* argv[] ) {
 
   // Set up thread block and grid
   int nb = 256;
-  dim3 grid  = ( (N+nb-1)/nb, 1, 1 ); // 1-D grid
-  dim3 block = ( nb, 1, 1 );          // 1-D block
+  dim3 grid  = dim3( (N+nb-1)/nb, 1, 1 ); // 1-D grid
+  dim3 block = dim3( nb, 1, 1 );          // 1-D block
+  cout << "Using grid: " << grid.x << ", " << grid.y << ", " << grid.z << endl;
+  cout << "Using block: " << block.x << ", " << block.y << ", " << block.z << endl;
 
   // Call kernel using triple chevron syntax
   // Note: Arguments are <<< grid, block, LDS_bytes, stream >>>
@@ -89,9 +96,13 @@ int main( int argc, char* argv[] ) {
       cerr << "Error: C[" << i << "] = " << h_C[i] << endl;
     }
   }
-  if( nerr > 0 )
+  if( nerr == 0 ) {
+    cout << "Success!" << endl;
+    cout << "h_C[" << N-1 << "] = " << h_C[N-1] << endl;
+  } else {
     cerr << "Total " << nerr << " errors." << endl;
-
+  }
+    
   // Clean up
   HIP_CHECK( hipFree( d_A ) );
   HIP_CHECK( hipFree( d_B ) );
