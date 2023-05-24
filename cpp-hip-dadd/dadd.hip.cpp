62388b42a7b77f42dd6c6d249b8027e8d9d976a8
Wileam Yonatan Phan <wil.phan@rice.edu>
2023-05-24 11:22:54 -0500

    Add makefile for HIP vector add code

--- cpp-hip-dadd/dadd.hip.cpp
+++ cpp-hip-dadd/dadd.hip.cpp
@@ -2,16 +2,31 @@
 #include <sstream>
 #include <hip/hip_runtime.h>
 
+#ifdef USE_ROCTX
+#include <roctracer/roctx.h>
+#endif /* USE_ROCTX */
+
 using namespace std;
 
 // Helper function for error checking
-#define HIP_CHECK(command) {                                  \
-  hipError_t status = command;                                \
-  if( status != hipSuccess ) {                                \
-  cerr << "HIP Error: " << hipGetErrorString(status) << endl; \
-  abort();                                                    \
-  }                                                           \
+#define HIP_CHECK(command) {                                    \
+  hipError_t status = command;                                  \
+  if( status != hipSuccess ) {                                  \
+    cerr << "HIP Error: " << hipGetErrorString(status) << endl; \
+  abort();                                                      \
+  }                                                             \
 }
+
+#ifdef USE_ROCTX
+#define ROCTX_CHECK(command) {               \
+  int stat = command;                        \
+  if (stat < 0) {                            \
+    cerr << "ROCTX Error: " << stat << endl; \
+  }                                          \
+}
+#else
+#define ROCTX_CHECK(command)
+#endif /* USE_ROCTX */
 
 // Kernel to add two vectors
 __global__ void dadd( const unsigned long N,
@@ -45,10 +60,13 @@ int main( int argc, char* argv[] ) {
     s >> N;
   } else {
     // Read N from standard input
+    std::cout << "Input vector length N: ";
     std::cin >> N;
     std::cout << "Using N = " << N << std::endl;
   } // argc, argv
 
+  ROCTX_CHECK( roctxRangePush("init") );
+  
   // Allocate vectors on host
   size_t sz_vec = N * sizeof(double);
   double* h_A = (double*)malloc( sz_vec );
@@ -73,13 +91,19 @@ int main( int argc, char* argv[] ) {
   HIP_CHECK( hipMalloc( &d_B, sz_vec ) );
   HIP_CHECK( hipMalloc( &d_C, sz_vec ) );
 
+  ROCTX_CHECK( roctxRangePush("h2d") );
+
   // Transfer data to device (synchronous blocking mode)
   // Note: hipMemcpy( dest, src, sz, direction )
   HIP_CHECK( hipMemcpy( d_A, h_A, sz_vec, hipMemcpyHostToDevice ) );
   HIP_CHECK( hipMemcpy( d_B, h_B, sz_vec, hipMemcpyHostToDevice ) );
 
+  ROCTX_CHECK( roctxRangePop() ); // h2d
+
   // Zero d_C using hipMemset()
   HIP_CHECK( hipMemset( d_C, 0, sz_vec ) );
+
+  ROCTX_CHECK( roctxRangePop() ); // init
 
   // Set up thread block and grid
   int nb = 256;
@@ -88,13 +112,20 @@ int main( int argc, char* argv[] ) {
   cout << "Using grid: " << grid.x << ", " << grid.y << ", " << grid.z << endl;
   cout << "Using block: " << block.x << ", " << block.y << ", " << block.z << endl;
 
+  ROCTX_CHECK( roctxRangePush("calc") );
+
   // Call kernel using triple chevron syntax
   // Note: Arguments are <<< grid, block, LDS_bytes, stream >>>
   //       For ROCm < 3.6, use hipLaunchKernelGGL()
   dadd<<< grid, block, 0, 0 >>>( N, d_A, d_B, d_C );
 
+  ROCTX_CHECK( roctxRangePop() ); // calc
+  ROCTX_CHECK( roctxRangePush("d2h") );
+  
   // Transfer data to host (synchronous blocking mode)
   HIP_CHECK( hipMemcpy( h_C, d_C, sz_vec, hipMemcpyDeviceToHost ) );
+
+  ROCTX_CHECK( roctxRangePop() ); // d2h
 
   // Check results
   int nerr = 0;
