c7c3a8419b8d3ea2bc826c3476dbb299b0d25169
Wileam Phan <wileam@phan.codes>
2024-12-24 06:49:50 -0800

    Port improvements from f90-acc-ddot
    Signed-off-by: Wileam Phan <wileam@phan.codes>

--- f90-ompto-ddot/ddot.f90
+++ f90-ompto-ddot/ddot.f90
@@ -1,23 +1,23 @@
 PROGRAM dot_product
+
+  ! Precision constants (Fortran 2008)
+  USE iso_fortran_env, ONLY: dp => real64, longlong => int64
+
   IMPLICIT NONE
 
-  ! Precision constants
-  INTEGER, PARAMETER :: dp = SELECTED_REAL_KIND(14,100) ! 64-bit (double)
-
   ! Vector dimension
-  INTEGER :: N
+  INTEGER(KIND=longlong) :: N
 
   ! Loop index
-  INTEGER :: i
+  INTEGER(KIND=longlong) :: i
 
   ! Vectors to take the dot product with
   REAL(KIND=dp), DIMENSION(:), ALLOCATABLE :: vecA, vecB
 
-  ! Result and check value
+  ! Result validation
+  INTRINSIC :: LOG
   REAL(KIND=dp) :: result, check
-
-  ! Tolerance for error checking
-  REAL(KIND=dp), PARAMETER :: tol = 10._dp**(-10)
+  REAL(KIND=dp), PARAMETER :: tol = 1.0E-7_dp
 
   ! Timing subroutine
   INTRINSIC :: DATE_AND_TIME
@@ -29,25 +29,25 @@ PROGRAM dot_product
   ! argc, argv (Fortran 2003)
   INTRINSIC :: COMMAND_ARGUMENT_COUNT, GET_COMMAND_ARGUMENT
   INTEGER :: argc
-  CHARACTER(LEN=8) :: argv ! We only need one
+  CHARACTER(LEN=12) :: argv ! We only need one
 
   ! Check argc
   IF( COMMAND_ARGUMENT_COUNT() > 0 ) THEN
 
      ! Read n from argv[1]
      CALL GET_COMMAND_ARGUMENT( 1, VALUE=argv )
-     READ( argv, * ) n
+     READ( argv, * ) N
 
   ELSE
 
      ! Read n from standard input
-     WRITE(*,*) 'Input vector length n:'
-     READ(*,*) n
+     WRITE(*,*) 'Input vector length N:'
+     READ(*,*) N
 
   END IF
 
   ! Echo n to standard output
-  WRITE(*,*) 'Using n = ', n
+  WRITE(*,*) 'Using N = ', N
 
   CALL DATE_AND_TIME( values=t0 )
 
@@ -56,7 +56,7 @@ PROGRAM dot_product
   ALLOCATE( vecB(n) )
 
   ! Allocate vectors and result variable on device (GPU)
-  !$OMP TARGET ENTER DATA MAP(ALLOC: vecA(1:N), vecB(1:N), result )
+  !$OMP TARGET DATA MAP(ALLOC: vecA(1:N), vecB(1:N), result )
 
   ! Initialize vectors on device
   ! Note: the order of the clauses matter!
@@ -85,28 +85,22 @@ PROGRAM dot_product
   END DO ! i
   !$OMP END TARGET TEAMS DISTRIBUTE PARALLEL DO
 
-!DEBUG
-  !$OMP TARGET
-  !result = 42._dp
-  !$OMP END TARGET
-!DEBUG
-  
-  ! Fetch result
-  !$OMP TARGET UPDATE FROM( result )
-  
   CALL DATE_AND_TIME( values=t2 )
 
-  ! Fetch result from device and close off data region
-  !$OMP TARGET EXIT DATA MAP(DELETE: vecA, vecB, result )
+  ! Fetch result from device
+  !$OMP TARGET UPDATE FROM( result )
 
-  ! Check value ( using relative error ) and print result to standard output
-  check = REAL( N, KIND=dp ) * REAL( N+1, KIND=dp ) * REAL( 2*N+1, KIND=dp) &
-          / 3._dp
-  IF( ABS( result/check - 1 ) > tol ) THEN
-     WRITE(*,*) 'Error! Result = ', result, ' when it should be ', check
+  ! Validate result using logarithmic error
+  check = LOG(REAL(N, KIND=dp)) + LOG(REAL(N, KIND=dp)+1) &
+          + LOG(2*REAL(N, KIND=dp)+1) - LOG(3._dp)
+  IF( ABS( LOG(result) - check ) > tol ) THEN
+     WRITE(*,*) 'Error! log(result) = ', LOG(result), ' when it should be ', check
   ELSE
      WRITE(*,*) 'Success! Result = ', result
   END IF
+
+  ! Close off data region
+  !$OMP END TARGET DATA
 
   ! Calculate and display timers
   time_init = REAL( 3600*t1(5) + 60*t1(6) + t1(7), KIND=dp ) &
